5630b787b9a0c6121c516eadb6e858f1b5676184
sjyptr <patidarsanjay88@gmail.com>
2021-09-24 09:47:25 +0530

    counter app change color

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from "react";
-import "./style.css";
 
-export default function App() {
+import React from 'react';
 
-  
+const handleColor = (x) => {
+  let y = 'red';
+  if (x > 0) {
+    y = 'blue';
+  } else if (x < 0) {
+    y = 'orange';
+  }
+  return y;
+};
+
+function App() {
+  // state variables, which can be changed in the course of the program.
+  const [count, setCount] = React.useState(0);
+  const [myColor, setMyColor] = React.useState('red');
+  const handleClick = (e) => {
+    let val = 0;
+    if (e.target.classList.contains('subtract')) {
+      val = count - 1;
+    } else if (e.target.classList.contains('add')) {
+      val = count + 1;
+    }
+    setCount(val);
+    const color = handleColor(val);
+    setMyColor(color);
+  };
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className='container'>
+      <div className='counter-div'>
+        <h1>Counter Application</h1>
+        <div
+          className='count'
+          style={{ color: myColor, fontSize: '24px', fontWeight: 'bold' }}
+        >
+          {count}
+        </div>
+        <hr />
+        <div className='buttons' onClick={handleClick}>
+          <button className='subtract'>Subtract</button>
+          <button className='reset'>Reset</button>
+          <button className='add'>Add</button>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default App;
